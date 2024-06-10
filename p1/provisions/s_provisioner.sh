8d09963e2519c11316c93742e13efcc27741e367
vmmon-th0 <ammonschiffer@outlook.com>
2024-06-10 23:50:35 +0200

    feat: p1 WIP

---
+++ p1/provisions/s_provisioner.sh
@@ -0,0 +1,23 @@
+cat >/etc/motd <<'EOF'
+  _    ____
+ | |  |___ \
+ | | __ __) |___
+ | |/ /|__ </ __|
+ |   < ___) \__ \
+ |_|\_\____/|___/   _____ _ __
+ / __|/ _ \ '__\ \ / / _ \ '__|
+ \__ \  __/ |   \ V /  __/ |
+ |___/\___|_|    \_/ \___|_|
+EOF
+
+echo "[k42s] firewalld disabled"
+systemctl disable firewalld --now
+
+echo "[k42s] install packages"
+yum install net-tools
+
+echo "[k42s] k3s installation on node"
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110" K3S_KUBECONFIG_MODE="644" sh -s - --flannel-backend none --token 12345
+
+echo "[k42s] server token are shared via synced_folder technique"
+sudo cp /var/lib/rancher/k3s/server/token /home/vagrant/shared
---
+++ p1/provisions/sw_provisioner.sh
@@ -0,0 +1,22 @@
+cat >/etc/motd <<'EOF'
+  _    ____
+ | |  |___ \
+ | | __ __) |___
+ | |/ /|__ </ __|        _
+ |   < ___) \__ \       | |
+ |_|\_\____/|___/_ _ __ | |_
+  / _` |/ _` |/ _ \ '_ \| __|
+ | (_| | (_| |  __/ | | | |_
+  \__,_|\__, |\___|_| |_|\__|
+         __/ |
+        |___/
+EOF
+
+echo "[k42s] firewalld disabled"
+systemctl disable firewalld --now
+
+echo "[k42s] install packages"
+yum install net-tools
+
+echo "[k42s] k3s installation on node"
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token-file /home/vagrant/shared/token --node-ip=192.168.56.111" K3S_KUBECONFIG_MODE="644" sh -s -
