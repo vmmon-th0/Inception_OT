8265a436f6f30690850e05cbb3f6897389d4a5b9
vmmon-th0 <ammonschiffer@outlook.com>
2024-06-30 16:25:46 +0200

    feat: p2 in wip

--- p2/provisions/s_provisioner.sh
+++ p2/provisions/s_provisioner.sh
@@ -9,11 +9,23 @@ echo "[k42s] firewalld disabled"
 systemctl disable firewalld --now
 
 echo "[k42s] install packages"
-yum install -y net-tools
 yum install -y nano
-
-echo "[k42s] manifest file management"
-mkdir -p /var/lib/rancher/k3s/server/manifests/
+yum install -y net-tools
 
 echo "[k42s] k3s installation on node"
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=$MASTERNODE_IP --flannel-iface=eth1" K3S_KUBECONFIG_MODE="644" sh -s - --token 12345
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=$MASTERNODE_IP --flannel-iface=eth1" K3S_KUBECONFIG_MODE="644" sh -s -
+
+echo "[k42s] Workload Deployment app1"
+# kubectl apply -f /home/vagrant/shared/app1.deployment.yaml
+# kubectl apply -f /home/vagrant/shared/app1.service.yaml
+# kubectl apply -f /home/vagrant/shared/app1.ingress.yaml
+
+echo "[k42s] Workload Deployment app2"
+# kubectl apply -f /home/vagrant/shared/app1.deployment.yaml
+# kubectl apply -f /home/vagrant/shared/app1.service.yaml
+# kubectl apply -f /home/vagrant/shared/app1.ingress.yaml
+
+echo "[k42s] Workload Deployment app3"
+# kubectl apply -f /home/vagrant/shared/app1.deployment.yaml
+# kubectl apply -f /home/vagrant/shared/app1.service.yaml
+# kubectl apply -f /home/vagrant/shared/app1.ingress.yaml
